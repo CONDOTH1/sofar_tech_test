e31c44ff57c88f4eece3971891dac8bf777382f9
Thady Condon <thadycondon@hotmail.com>
2016-10-21 12:19:51 +0100

    Decoupled the artist from the create method in the music_from_api class

--- lib/music_from_api.rb
+++ lib/music_from_api.rb
@@ -19,22 +19,30 @@ class MusicFromApi
       if video["song"].include?("artist")
         if video["song"].include?("city")
 
-          artist = video["song"]["artist"]["title"]
-          new_artist = Artist.create(title: artist) if !Artist.exists?(title: artist)
+          artist(video)
 
           city = video["song"]["city"]["title"]
           new_city = City.create(city_title: city) if !City.exists?(city_title: city)
 
-          find_artist = Artist.find_by(title: artist)
           find_city = City.find_by(city_title: city)
 
           music = video["song"]["title"]
-          new_song = find_artist.songs.create(title: music, rating: 5, city_id: find_city.id)
+          new_song = @artist.songs.create(title: music, rating: 5, city_id: find_city.id)
 
           new_video = new_song.create_video(video_title: video_param)
         end
       end
     end
+  end
+
+  def artist(video)
+    artist_title = video["song"]["artist"]["title"]
+    Artist.create(title: artist_title) if !Artist.exists?(title: artist_title)
+    find_artist(artist_title)
+  end
+
+  def find_artist(artist_title)
+    @artist = Artist.find_by(title: artist_title)
   end
 
 end
