99acfe7572dc4be507583525352837ddc41734ee
Thady Condon <thadycondon@hotmail.com>
2016-10-21 11:23:17 +0100

    Removed logic from bloated controller method music_api to lib directory. The new class in the lib directory is called music_from_api and manages the data from the api

---
+++ lib/music_from_api.rb
@@ -0,0 +1,39 @@
+# require './app/models/artist'
+# require './../app/models/city'
+# require './../app/models/song'
+
+
+class MusicFromApi
+
+  def initialize(response)
+    @response = response
+    create
+  end
+
+  def create
+    @response.each do |video|
+    video_param = video["video_uid"]
+      if video.include?("song")
+        if video["song"].include?("artist")
+          if video["song"].include?("city")
+
+            artist = video["song"]["artist"]["title"]
+            new_artist = Artist.create(title: artist) if !Artist.exists?(title: artist)
+
+            city = video["song"]["city"]["title"]
+            new_city = City.create(city_title: city) if !City.exists?(city_title: city)
+
+            find_artist = Artist.find_by(title: artist)
+            find_city = City.find_by(city_title: city)
+
+            music = video["song"]["title"]
+            new_song = find_artist.songs.create(title: music, rating: 5, city_id: find_city.id)
+
+            new_video = new_song.create_video(video_title: video_param)
+          end
+        end
+      end
+    end
+  end
+
+end
